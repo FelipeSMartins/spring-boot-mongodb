53c74097ccf0e0054a4e3c09065b2ed4f6cde5de
Felipe Martins <felipemartins.fab@gmail.com>
2023-10-25 22:08:40 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/project/mongodbproject/MongodbprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.mongodbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbprojectApplication.class, args);
+	}
+
+}
